ac383bbf0159540f45f6af4645b2f2ec2216b3fd
smitbuha25 <130464705+smitbuha25@users.noreply.github.com>
2023-05-26 05:49:00 -0700

    Add files via upload

---
+++ smit TX=4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b,n;
+	
+	printf("Enter the 1st number =>");
+	scanf("%d",&n);
+	
+	printf("Enter the 2st number =>");
+	scanf("%d",&n);
+	
+	printf("Enter the 3st number =>");
+	scanf("%d",&n);
+	
+	printf("Enter the 4st number =>");
+	scanf("%d",&n);
+	
+	if (a<b)
+	{
+		if(b<c)
+		{
+			if(c>a)
+			{
+				printf("%d is max");
+			}
+		}
+	}
+	else
+    {
+	    if(c>a)
+	    {
+		    if(c>b)
+			{
+			    if(b<a)
+			    {
+			    	printf("%d is maxmum");
+				}
+}
---
+++ smit TX=5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,a;
+	
+	for(i=a;i<=1;i++)
+	{
+		for(j=5;j<=1;j--);
+		{
+			printf("%c",a+64);
+		}
+	}
+}
